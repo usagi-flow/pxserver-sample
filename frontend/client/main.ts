60917c039694f546fe6858bd30d0721c2a39ed99
etienne-k <freydaklin@gmail.com>
2019-11-30 21:59:32 +0100

    Stabilized sample application

--- frontend/client/main.ts
+++ frontend/client/main.ts
@@ -2,12 +2,6 @@ import "./styles.scss";
 
 import Vue from "vue";
 
-import AppContent from "./app-content/app-content";
-import VueComponent from "./vue-component.vue";
-
-AppContent.register();
-Vue.component("VueComponent", VueComponent);
-
 let app : Vue = new Vue({
 	el: "#app",
 	data: {
